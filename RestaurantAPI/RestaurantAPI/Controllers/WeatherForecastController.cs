0683ec5ed3841c1d85eaeebb9ebcb52faf091b8c
Sebastian Krzemień <skrzemien@outlook.com>
2024-05-20 19:45:05 +0200

    Few lessons today

--- RestaurantAPI/RestaurantAPI/Controllers/WeatherForecastController.cs
+++ RestaurantAPI/RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -11,29 +11,32 @@ namespace RestaurantAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+        private readonly IWeatherForecastService _service;
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService service)
         {
             _logger = logger;
+            _service = service;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var result = _service.Get();
+            return result;
+        }
+
+        [HttpGet("currentDay/{max}")]
+        public IEnumerable<WeatherForecast> Get2([FromQuery]int take, [FromRoute]int max)
+        {
+            var result = _service.Get();
+            return result;
+        }
+        [HttpPost]
+        public ActionResult<string> Hello([FromBody] string name)
+        {
+            return NotFound($"Hello {name}");
         }
     }
 }
---
+++ RestaurantAPI/RestaurantAPI/IWeatherForecastService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RestaurantAPI
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get();
+    }
+}
